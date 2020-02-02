4deadf0edf26f093b835b791130c23b041abc0aa
uzihoon <Uzihoon.dev@gmail.com>
2020-02-02 06:36:55 +0000

    set init jx

--- create_eks.sh
+++ create_eks.sh
@@ -1,6 +1,6 @@
 export AWS_ACCESS_KEY_ID=$(aws configure get aws_acces_key_id)
 export AWS_SECRET_ACCESS_KEY=$(aws configure get aws_secret_access_key)
 
-jx create cluster eks --cluster-name=jenkins-test --skip-installation-true
+jx create cluster eks --cluster-name=jenkins-test --skip-installation=true
 
 eksctl get clusters
---
+++ init_jx.sh
@@ -0,0 +1,2 @@
+jx install --provider=eks --domain=konsek.cloud --default-environment-prefix=konsek-cloud
+
